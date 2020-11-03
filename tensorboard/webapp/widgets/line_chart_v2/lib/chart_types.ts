2effde40fb594a78102c27a11762a39a9a558b74
Stephan Lee <stephanwlee@gmail.com>
2020-11-03 13:47:26 -0800

    line chart: add the library entry point
    This change adds  that glues coordinator, view, and renderer.

---
+++ tensorboard/webapp/widgets/line_chart_v2/lib/chart_types.ts
@@ -0,0 +1,46 @@
+import {
+  DataSeries,
+  DataSeriesMetadataMap,
+  Dimension,
+  Extent,
+} from './internal_types';
+import {RendererType} from './renderer/renderer_types';
+import {ScaleType} from './scale_types';
+
+export interface IChart {
+  resize(dim: Dimension): void;
+
+  updateMetadata(metadataMap: DataSeriesMetadataMap): void;
+
+  updateViewBox(extent: Extent): void;
+
+  updateData(data: DataSeries[]): void;
+
+  setXScaleType(type: ScaleType): void;
+
+  setYScaleType(type: ScaleType): void;
+
+  dispose(): void;
+}
+
+export interface ChartCallbacks {
+  onDrawEnd(): void;
+}
+
+export interface BaseChartOption {
+  callbacks: ChartCallbacks;
+  domDimension: Dimension;
+}
+
+export interface SvgChartOption extends BaseChartOption {
+  type: RendererType.SVG;
+  container: SVGElement;
+}
+
+export interface WebGlChartOption extends BaseChartOption {
+  type: RendererType.WEBGL;
+  devicePixelRatio: number;
+  container: OffscreenCanvas | HTMLCanvasElement;
+}
+
+export type ChartOption = SvgChartOption | WebGlChartOption;
